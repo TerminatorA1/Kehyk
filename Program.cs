4a3116b01ab85f2cc513b4f25268d06261e50867
TerminatorA1 <62852208+TerminatorA1@users.noreply.github.com>
2020-11-13 13:35:09 +0200

    Add files via upload
    t1-01

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace viikko1_t1_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 + 5);
+        }
+    }
+}
