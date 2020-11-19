dac3a17d56cf94f36c645e8b57b438f581efb59c
TerminatorA1 <62852208+TerminatorA1@users.noreply.github.com>
2020-11-19 14:35:47 +0200

    valmiit ohjelmat 18-11

---
+++ Viikko1_T1-02/ProgramV1T102.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Viikko1_T1_02
+{
+    class ProgramV1T102
+    {
+        static void Main(string[] args)
+        {
+            int celsius = 10; // lämpötila celsius asteina
+            Console.WriteLine(celsius*1.8 + 32); // tulostus ruudulle, lämpötila fahrenheit
+        }
+    }
+}
