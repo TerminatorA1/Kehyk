832a294fb8ee533044af7127db07707adf670996
TerminatorA1 <62852208+TerminatorA1@users.noreply.github.com>
2020-11-27 14:01:19 +0200

    Valmiit tehtävät 27-11

---
+++ V4-Kirjautumisikkuna/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace V4_Kirjautumisikkuna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.KayttajaLB = new System.Windows.Forms.Label();
+            this.KayttajaTB = new System.Windows.Forms.TextBox();
+            this.SalasanaLB = new System.Windows.Forms.Label();
+            this.SalasanaTB = new System.Windows.Forms.TextBox();
+            this.KirjauduPainike = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kirjautumisikkuna";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // KayttajaLB
+            // 
+            this.KayttajaLB.AutoSize = true;
+            this.KayttajaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KayttajaLB.Location = new System.Drawing.Point(53, 199);
+            this.KayttajaLB.Name = "KayttajaLB";
+            this.KayttajaLB.Size = new System.Drawing.Size(135, 24);
+            this.KayttajaLB.TabIndex = 1;
+            this.KayttajaLB.Text = "Käyttäjätunnus:";
+            // 
+            // KayttajaTB
+            // 
+            this.KayttajaTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KayttajaTB.Location = new System.Drawing.Point(201, 199);
+            this.KayttajaTB.Name = "KayttajaTB";
+            this.KayttajaTB.Size = new System.Drawing.Size(158, 29);
+            this.KayttajaTB.TabIndex = 2;
+            // 
+            // SalasanaLB
+            // 
+            this.SalasanaLB.AutoSize = true;
+            this.SalasanaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalasanaLB.Location = new System.Drawing.Point(97, 256);
+            this.SalasanaLB.Name = "SalasanaLB";
+            this.SalasanaLB.Size = new System.Drawing.Size(91, 24);
+            this.SalasanaLB.TabIndex = 3;
+            this.SalasanaLB.Text = "Salasana:";
+            // 
+            // SalasanaTB
+            // 
+            this.SalasanaTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalasanaTB.Location = new System.Drawing.Point(201, 256);
+            this.SalasanaTB.Name = "SalasanaTB";
+            this.SalasanaTB.Size = new System.Drawing.Size(158, 29);
+            this.SalasanaTB.TabIndex = 4;
+            this.SalasanaTB.UseSystemPasswordChar = true;
+            // 
+            // KirjauduPainike
+            // 
+            this.KirjauduPainike.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KirjauduPainike.Location = new System.Drawing.Point(221, 315);
+            this.KirjauduPainike.Name = "KirjauduPainike";
+            this.KirjauduPainike.Size = new System.Drawing.Size(138, 32);
+            this.KirjauduPainike.TabIndex = 5;
+            this.KirjauduPainike.Text = "Kirjaudu";
+            this.KirjauduPainike.UseVisualStyleBackColor = true;
+            this.KirjauduPainike.Click += new System.EventHandler(this.KirjauduPainike_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::V4_Kirjautumisikkuna.Properties.Resources.iconfinder_user_alt_285645;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.KirjauduPainike);
+            this.Controls.Add(this.SalasanaTB);
+            this.Controls.Add(this.SalasanaLB);
+            this.Controls.Add(this.KayttajaTB);
+            this.Controls.Add(this.KayttajaLB);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kirjautumisikkuna";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label KayttajaLB;
+        private System.Windows.Forms.TextBox KayttajaTB;
+        private System.Windows.Forms.Label SalasanaLB;
+        private System.Windows.Forms.TextBox SalasanaTB;
+        private System.Windows.Forms.Button KirjauduPainike;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ V4-Kirjautumisikkuna/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V4_Kirjautumisikkuna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KirjauduPainike_Click(object sender, EventArgs e)
+        {
+            String kayttaja = KayttajaTB.Text;
+            String salasana = SalasanaTB.Text;
+            if(kayttaja == "avanhatalo" && salasana == "Keuda")
+            {
+                this.Hide();
+                Paaikkuna paa = new Paaikkuna();
+                paa.Show();
+            }
+            else
+            {
+                MessageBox.Show("Käyttäjätunnus tai salasana on väärin", "Virheilmoitus");
+            }
+        }
+    }
+}
